0de27a2ea1b18046ab0c023f2d76768971eafc43
tpluscode <tpluscode@users.noreply.github.com>
2019-07-10 09:54:34 +0200

    chore: this whitespace keeps getting regenerated

--- app.hypermedia.testing.dsl.web/src/main/webapp/xtext-resources/generated/mode-api.js
+++ app.hypermedia.testing.dsl.web/src/main/webapp/xtext-resources/generated/mode-api.js
@@ -1,6 +1,6 @@
 define(["ace/lib/oop", "ace/mode/text", "ace/mode/text_highlight_rules"], function(oop, mText, mTextHighlightRules) {
 	var HighlightRules = function() {
-		var keywords = "Class|Expect|Property|With";
+		var keywords = "Class|Property";
 		this.$rules = {
 			"start": [
 				{token: "comment", regex: "\\/\\/.*$"},
