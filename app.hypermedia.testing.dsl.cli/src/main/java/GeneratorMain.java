3ef7d23b6d78365929b4f632a63e3c24d5869ecd
tpluscode <tpluscode@users.noreply.github.com>
2019-11-13 16:20:09 +0100

    fix: enforce UTF-8 encoding

--- app.hypermedia.testing.dsl.cli/src/main/java/GeneratorMain.java
+++ app.hypermedia.testing.dsl.cli/src/main/java/GeneratorMain.java
@@ -27,42 +27,41 @@ import app.hypermedia.testing.dsl.CoreStandaloneSetup;
 import app.hypermedia.testing.dsl.HydraStandaloneSetup;
 
 public class GeneratorMain {
-    
+
     private static Map<String, GeneratorRunner> runners = new HashMap<String, GeneratorRunner>();
-    
+
     public static void main(String[] args) {
         Injector coreInjector = new CoreStandaloneSetup().createInjectorAndDoEMFRegistration();
         Injector hydraInjector = new HydraStandaloneSetup().createInjectorAndDoEMFRegistration();
-        
+
         runners.put("api", coreInjector.getInstance(GeneratorRunner.class));
         runners.put("hydra", hydraInjector.getInstance(GeneratorRunner.class));
-        
+
         generateDirectory(args[0]);
         System.out.println("Done!");
     }
 
     private static void generateDirectory(String directoryName) {
         File dir = new File(directoryName);
-        Collection<File> files = FileUtils.listFiles(dir, new RegexFileFilter(".+\\.(api|hydra)$"), TrueFileFilter.INSTANCE);
+        Collection<File> files = FileUtils.listFiles(dir, new RegexFileFilter(".+\\.(api|hydra)$"),
+                TrueFileFilter.INSTANCE);
 
         for (File file : files) {
             if (file.isDirectory()) {
                 generateDirectory(file.getPath());
                 continue;
             }
-            
+
             String ext = FilenameUtils.getExtension(file.getPath());
-            
+
             GeneratorRunner runner = runners.get(ext);
-            
+
             if (runner != null) {
                 runner.generate(file);
             }
         }
     }
 }
-
-
 
 class GeneratorRunner {
     @Inject
@@ -76,7 +75,7 @@ class GeneratorRunner {
 
     @Inject
     JavaIoFileSystemAccess fileAccess;
-    
+
     public void generate(File input) {
         System.out.printf("Compiling %s %n", input.getPath());
 
@@ -94,10 +93,7 @@ class GeneratorRunner {
     private boolean validate(Resource resource) {
         List<Issue> issues = validator.validate(resource, CheckMode.ALL, CancelIndicator.NullImpl);
         if (!issues.isEmpty()) {
-            issues.forEach(issue -> System.out.printf(
-                    "Line %3s: %s %s %n",
-                    issue.getLineNumber(),
-                    issue.getSeverity(),
+            issues.forEach(issue -> System.out.printf("Line %3s: %s %s %n", issue.getLineNumber(), issue.getSeverity(),
                     issue.getMessage()));
             return false;
         }
--- app.hypermedia.testing.dsl/src/main/java/app/hypermedia/testing/dsl/HydraRuntimeModule.xtend
+++ app.hypermedia.testing.dsl/src/main/java/app/hypermedia/testing/dsl/HydraRuntimeModule.xtend
@@ -5,6 +5,9 @@ package app.hypermedia.testing.dsl
 
 import app.hypermedia.testing.dsl.conversion.HydraValueConverterService
 import org.eclipse.xtext.conversion.IValueConverterService
+import com.google.inject.Binder
+import org.eclipse.xtext.parser.IEncodingProvider
+import org.eclipse.xtext.service.DispatchingProvider
 
 /**
  * Use this class to register components to be used at runtime / without the Equinox extension registry.
@@ -13,4 +16,11 @@ class HydraRuntimeModule extends AbstractHydraRuntimeModule {
     override Class<? extends IValueConverterService> bindIValueConverterService() {
         return HydraValueConverterService
     }
+
+    override configureRuntimeEncodingProvider(Binder binder) {
+        binder.bind(IEncodingProvider)
+              .annotatedWith(DispatchingProvider.Runtime)
+              .to(AlwaysUtfEncodingProvider)
+    }
+
 }
